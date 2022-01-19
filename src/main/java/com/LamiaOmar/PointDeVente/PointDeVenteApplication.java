d41a0b0e29a4d77582e841eae70886353b3c34a6
lamia-el-azzouzi <lamia@weegolife.com>
2022-01-19 11:29:35 +0100

    Create project with Spring Initializer

---
+++ src/main/java/com/LamiaOmar/PointDeVente/PointDeVenteApplication.java
@@ -0,0 +1,13 @@
+package com.LamiaOmar.PointDeVente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointDeVenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointDeVenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LamiaOmar/PointDeVente/PointDeVenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LamiaOmar.PointDeVente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointDeVenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
